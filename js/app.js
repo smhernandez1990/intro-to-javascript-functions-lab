f259c028cf00c57c446eeb9811e1d73862e79e7e
smhernandez1990 <samanthamaria.hernandez90@gmail.com>
2025-11-18 23:41:06 -0500

    isAdult function

---
+++ js/app.js
@@ -0,0 +1,46 @@
+/*
+Exercise 1: maxOfTwoNumbers()
+
+In this exercise, create a function named maxOfTwoNumbers. 
+It should take two numbers as inputs and return the larger number. 
+If they're equal, return either one.
+
+Exercise 1 has been completed for you:
+*/
+
+const maxOfTwoNumbers = (x, y) => {
+    if (x >= y) {
+        return x;
+    } else {
+        return y;
+    }
+}
+
+console.log("Exercise 1 Result:", maxOfTwoNumbers(3, 9));
+
+/*
+Exercise 2: isAdult()
+
+Write a function named isAdult. It should take an age (number) 
+and return 'Adult' if the age is 18 or over and 'Minor' otherwise.
+
+Example: isAdult(21) should return 'Adult'.
+
+Complete the exercise in the space below:
+*/
+
+
+function isAdult(age) {
+    if(age >= 18){
+        return "Adult";
+    }
+    else if(age < 18){
+        return "Minor";
+    }
+    
+}
+
+console.log("Exercise 2 Result:", isAdult(21));
+
+
+
