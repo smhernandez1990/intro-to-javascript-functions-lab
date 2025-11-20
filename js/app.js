a4f4a7c61df2553f9c3e43dc23a24e44a71a8daf
smhernandez1990 <samanthamaria.hernandez90@gmail.com>
2025-11-19 22:16:18 -0500

    createUsername function and updated greetUser

--- js/app.js
+++ js/app.js
@@ -96,7 +96,7 @@ Complete the exercise in the space below:
 */
 
 function greetUser(name, time){
-    return "Good" + " " + (time) + " " + (name) + "!"
+    return `Good ${time} ${name}!`
 }
 
 
@@ -227,3 +227,25 @@ function calculateGrade(score){
 }
 
 console.log('Exercise 10 Result:', calculateGrade(85));
+
+/*
+Exercise 11: createUsername()
+
+Define a function called createUsername. 
+It should take a first name and a last name and return a username. 
+
+The username should be a combination of the following:
+- The first three letters of the first name.
+- The first three letters of the last name.
+- The total character count of the first and last name combined.
+
+Example: createUsername('Samantha', 'Green') should return 'SamGre13'.
+
+Complete the exercise in the space below:
+*/
+
+function createUsername(firstName, lastName){
+    return (firstName.slice(0, 3)) + (lastName.slice(0, 3)) + ((firstName + lastName).length)
+}
+
+console.log('Exercise 11 Result:', createUsername("Samantha", "Green"));
