fff9840aa621f64b25544c1a5f6fe9e78bb92d3d
smhernandez1990 <samanthamaria.hernandez90@gmail.com>
2025-11-19 22:41:38 -0500

    numArgs function

--- js/app.js
+++ js/app.js
@@ -250,3 +250,17 @@ function createUsername(firstName, lastName){
 
 console.log('Exercise 11 Result:', createUsername("Samantha", "Hernandez"), 'hey its me!');
 
+/*
+Exercise 12: numArgs()
+
+Challenge yourself with numArgs. 
+This function should return the count of arguments passed to it when called.
+
+Complete the exercise in the space below:
+*/
+
+function numArgs(){
+    return arguments.length;
+}
+
+console.log('Exercise 12 Result:', numArgs(1, 2, 3, 4));
